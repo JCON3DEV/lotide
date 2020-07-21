a4e92f500ce5ced689bac7208c0bc7278e921d8b
JCON3DEV <jconheasman@gmail.com>
2020-07-21 05:20:32 +0000

    function created to debug using console.assert method

---
+++ assertEqual.js
@@ -0,0 +1,25 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  const emoji = require('node-emoji');
+
+  const x = emoji.get('x');
+  const thumbsup = emoji.get('thumbsup');
+
+  if (actual === expected) {
+    console.log(thumbsup + " " + "Assertion Passed: " + actual + " " + "===" + " " + expected);
+  } else {
+    console.log(x + " " + "Assertion Failed: " + actual + " " + "!==" + " " + expected);
+  }
+
+};
+
+//could also use String.fromCodePoint() to make emojis
+// eg; var emoji = String.fromCodePoint(0x1F621)
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Jordan", "Jordan");
+assertEqual("Jordan", "Badass");
+assertEqual(16, 12);
+assertEqual(94, "Golf");
