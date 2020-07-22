57efcddbef21719b5d54916652573e15370594c0
JCON3DEV <jconheasman@gmail.com>
2020-07-22 05:47:28 +0000

    first commit. Do not understand the objective.

---
+++ assertArraysEqual.js
@@ -0,0 +1,41 @@
+const assertEqual = function (actual, expected) {
+
+  console.log(expected);
+  const emoji = require('node-emoji');
+
+  const x = emoji.get('x');
+  const thumbsup = emoji.get('thumbsup');
+
+  if (actual === expected) {
+    console.log(`${thumbsup} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${x} Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const eqArrays = function (array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+
+    if (array1[i] === array2[i]) {
+      //console.log(array1[i], array2[i])
+      //console.log("matches");
+      expected = true;
+    }
+    else {
+      //console.log("false");
+      expected = false;
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const assertArraysEqual = (first, second)=> {
+  array1 = first;
+  array2 = second;
+  console.log("whats going on?");
+};
+//Tired brain. What does this challenge want me to do??......
+assertArraysEqual([1,2,3,4],[1,2,3,4]);
