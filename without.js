b695225ed68d9a322dbf7252ea545d5c537e1bbb
JCON3DEV <jconheasman@gmail.com>
2020-07-22 22:27:53 +0000

    changed approach completely. Used filter method, as well as includes method. Oh my jeez what an improvement.

--- without.js
+++ without.js
@@ -38,17 +38,13 @@ const assertArraysEqual = (first, second) => {
 };
 
 const without = function(source, itemsToRemove) {
-  let holdingArray = [];
-  for (let i = 0; i < source.length; i++) {
-    
-    if (source[i] === itemsToRemove[i]) {
-      
-    }
-    else {
-      holdingArray = holdingArray.concat(source[i]);
-    }
-  }
+  //console.log(source);
+  //console.log(itemsToRemove);
+  
+  let holdingArray = source.filter(word => !itemsToRemove.includes(word));
   return holdingArray;
+  //realised that filter would be easier. Mentor Monica helped me to find 
+  // .includes() and then from there got this really concise result!
 };
 
 console.log(without([1, 2, 3], [1])); // => [2, 3]
@@ -56,20 +52,3 @@ console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
 
 const words = ["hello", "world", "lighthouse"];
 console.log(without(words, ["lighthouse"])); 
-//###Need to loop through each item in the itemToRemove array so that it needn'y be in the same order##
-
-// no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-
-
-/* --- challenge states: ---
-Implement without which will return a subset of a given array, 
-removing unwanted elements. 
-This function should take in a source array and a itemsToRemove array.
-It should return a new array with only those elements from source 
-that are not present in the itemsToRemove array.
-
-## it does not say that these sould be passed to any other function 
-or am i mistaken? ##
-*/
