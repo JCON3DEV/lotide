0dfae42e7c92b4208fc66f48cffb653d3fc60f42
JCON3DEV <jconheasman@gmail.com>
2020-07-22 20:47:46 +0000

    Justin really helped. currently the itemsToRemove need to match both value and index in order to be removed

--- without.js
+++ without.js
@@ -33,19 +33,19 @@ const assertArraysEqual = (first, second) => {
       return false;
     }
   }
-
+  
   return true;
 };
 
 const without = function(source, itemsToRemove) {
-  let holdingArray = "";
+  let holdingArray = [];
   for (let i = 0; i < source.length; i++) {
-
+    
     if (source[i] === itemsToRemove[i]) {
-      source[i]++;
+      
     }
     else {
-      holdingArray = holdingArray + source[i]
+      holdingArray = holdingArray.concat(source[i]);
     }
   }
   return holdingArray;
@@ -56,6 +56,8 @@ console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
 
 const words = ["hello", "world", "lighthouse"];
 console.log(without(words, ["lighthouse"])); 
+//###Need to loop through each item in the itemToRemove array so that it needn'y be in the same order##
+
 // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
