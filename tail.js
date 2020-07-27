3571c7bfad72852006bad9d926394efadb138a71
JCON3DEV <jconheasman@gmail.com>
2020-07-27 22:15:41 +0000

    refactored tail.js and added a test file for it

--- tail.js
+++ tail.js
@@ -1,20 +1,25 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  const emoji = require('node-emoji');
+// // // FUNCTION IMPLEMENTATION
+// const assertEqual = function(actual, expected) {
+//   const emoji = require('node-emoji');
 
-  const x = emoji.get('x');
-  const thumbsup = emoji.get('thumbsup');
+//   const x = emoji.get('x');
+//   const thumbsup = emoji.get('thumbsup');
 
-  if (actual === expected) {
-    console.log(`${thumbsup} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${x} Assertion Failed: ${actual} !== ${expected}`);
-  }
+//   if (actual === expected) {
+//     console.log(`${thumbsup} Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`${x} Assertion Failed: ${actual} !== ${expected}`);
+//   }
 
-};
+// };
 
 //could also use String.fromCodePoint() to make emojis
 // eg; var emoji = String.fromCodePoint(0x1F621)
+
+// ==============
+/* tail() should return an array containing everything form 
+the original except the first element */
+// =============
 
 function tail(newArray) {
   let tailArray = [];
@@ -26,19 +31,11 @@ function tail(newArray) {
   
 }
 
-// TEST CODE
+module.exports = tail;
+// module.exports = assertEqual;
 
-tail([2, 4, 6, 8]);
+
 // Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-//assertEqual(words.length, 3); // original array should still have 3 elements!
-
-
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Jordan", "Jordan");
-assertEqual("Jordan", "Badass");
-assertEqual(16, 12);
-assertEqual(94, "Golf");
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// //assertEqual(words.length, 3); // original array should still have 3 elements!
---
+++ test/tail.test.js
@@ -0,0 +1,8 @@
+const tail = require("../tail.js");
+// const asserEqual = require("../assertEqual.js");
+
+// TEST CODE
+
+tail([2, 4, 6, 8]);
+tail([4, 2, 8, 5, 3]);
+tail([1, 2, 3, 4, 5]);
