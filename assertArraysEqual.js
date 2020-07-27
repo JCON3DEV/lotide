5f11fede19afe95cad2a10eeea7e988d106150ff
JCON3DEV <jconheasman@gmail.com>
2020-07-27 23:05:36 +0000

    refactored assertArraysEqual and created a seperate testing file for it

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,29 +1,13 @@
-const assertEqual = function (actual, expected) {
-
-  console.log(expected);
-  const emoji = require('node-emoji');
-
-  const x = emoji.get('x');
-  const thumbsup = emoji.get('thumbsup');
-
-  if (assertArraysEqual(actual, expected)){
-    console.log(`${thumbsup} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${x} Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
+/* This function returns a boolean true or false if the arrays 
+are the same length  */
 const assertArraysEqual = (first, second)=> {
-  array1 = first;
-  array2 = second;
-  if (array1 === undefined) {
+  if (first === undefined) {
     return true;
   }
   
-  for (let i = 0; i < array1.length; i++) {
+  for (let i = 0; i < first.length; i++) {
 
-    if (array1[i] === array2[i]) {
+    if (first[i] === second[i]) {
       //console.log(array1[i], array2[i])
       //console.log("matches");
       expected = true;
@@ -38,10 +22,7 @@ const assertArraysEqual = (first, second)=> {
   return true;
 };
 
-/* 
-below was original approach which worked - subsequently changed a few lines of 
-code within assertArraysEqual
-*/
-//assertEqual(assertArraysEqual([1,5,3,4],[1,2,3,4]), expected);
-assertEqual([1, 5, 3, 4], [1, 2, 3, 4]);
+module.exports = assertArraysEqual;
+
+
 
---
+++ test/assertArraysEqual.test.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = require("../assertArraysEqual.js");
+const assertEqual = require("../assertEqual.js");
+
+// Test code;
+console.log(assertEqual(assertArraysEqual([1, 5, 3, 4], [1, 2, 3, 4]), expected));
+console.log(assertEqual([1, 5, 3, 4], [1, 2, 3, 4]));
+
